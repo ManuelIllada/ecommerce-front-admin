4b0c86aa0f434128ae5e5c991e50026d9fefd786
pablinuxx <74331843+pablinuxx@users.noreply.github.com>
2023-04-06 02:17:09 -0300

    modif y eliminar user

--- src/components/Users/TableUsers.jsx
+++ src/components/Users/TableUsers.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillEdit } from "react-icons/ai";
 import { BsFillTrashFill } from "react-icons/bs";
 import { useFetch } from "../../hooks/useFetch";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { toast } from "react-hot-toast";
 
 const TableUsers = () => {
+  const notifySuccess = (message) =>
+    toast.success(message, {
+      duration: 2000,
+      position: "bottom-right",
+    });
+  const notifyError = (error) =>
+    toast.error(error, {
+      duration: 2000,
+      position: "bottom-right",
+    });
+
+  const notifyisDenied = ({ message }) =>
+    toast(message, {
+      duration: 2000,
+      position: "bottom-right",
+    });
+
   const { data } = useFetch(`${process.env.REACT_APP_API_URL}/users`);
+  const [usersList, setUsersList] = useState(null);
+
+  useEffect(() => {
+    setUsersList(data);
+  }, [data]);
+
+  const handleDelete = async (event, user) => {
+    event.preventDefault();
+
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/users/${user.id}`,
+      {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((res) => res.json());
+    if (response.message) {
+      notifySuccess(response.message);
+      setUsersList(usersList.filter((u) => u.id !== user.id));
+    } else {
+      notifyError(response.error);
+    }
+  };
+
   return (
     <>
       <div className="row m-3">
@@ -29,8 +72,8 @@ const TableUsers = () => {
           </tr>
         </thead>
         <tbody>
-          {data &&
-            data.map((user) => (
+          {usersList &&
+            usersList.map((user) => (
               <tr key={user.id} className="text-center">
                 <td>{user.id}</td>
                 <td>
@@ -46,8 +89,32 @@ const TableUsers = () => {
                 </td>
                 <td>{user.email}</td>
                 <td className="d-flex justify-content-around">
-                  <AiFillEdit className="text-primary" />
-                  <BsFillTrashFill className="text-danger" />
+                  <Link to="/users/edit" state={user}>
+                    <AiFillEdit className="text-primary" />
+                  </Link>
+                  <Link>
+                    <BsFillTrashFill
+                      className="text-danger"
+                      onClick={(event) =>
+                        Swal.fire({
+                          text: "Esta seguro que desea eliminar esta categoria?",
+                          icon: "error",
+                          showDenyButton: true,
+                          denyButtonText: "No",
+                          confirmButtonText: "Si",
+                        }).then((response) => {
+                          if (response.isDenied) {
+                            notifyisDenied({
+                              message: "ℹ️ Accion negada por el usuario",
+                              type: "",
+                            });
+                          } else {
+                            handleDelete(event, user);
+                          }
+                        })
+                      }
+                    />
+                  </Link>
                 </td>
               </tr>
             ))}
--- src/components/Users/UsersModify.jsx
+++ src/components/Users/UsersModify.jsx
@@ -1,7 +1,157 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
+import axios from "axios";
 const UsersModify = () => {
-  return <div>UsersModify</div>;
+  const notifySuccess = (message) =>
+    toast.success(message, {
+      duration: 2000,
+      position: "bottom-right",
+    });
+  const notifyError = (error) =>
+    toast.error(error, {
+      duration: 3000,
+      position: "bottom-right",
+    });
+  const navigate = useNavigate();
+
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const location = useLocation();
+
+  const handleSendModify = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("firstname", firstname);
+    formData.append("lastname", lastname);
+    formData.append("email", email);
+    formData.append("address", address);
+    formData.append("password", password);
+    formData.append("phone", phone);
+    formData.append("avatar", avatar);
+
+    const response = await axios({
+      url: `http://localhost:8000/users/${location.state.id}`,
+      method: "PATCH",
+      data: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    response.data.message
+      ? notifySuccess(response.data.message)
+      : notifyError(response.data.error);
+
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h3>Ingrese Datos a Modifdicar de User</h3>
+
+        <form onSubmit={handleSendModify} method="POST">
+          <div className="mb-3 d-flex justify-content-around">
+            <label htmlFor="id" className="form-label">
+              ID:
+            </label>
+            <input
+              type="text"
+              className="form-control w-25"
+              disabled
+              value={location.state.id}
+            />
+          </div>
+          <div className="mb-3 d-flex justify-content-around">
+            <label htmlFor="id" className="form-label">
+              Firstname:
+            </label>
+            <input
+              className="form-control w-25"
+              name="firstname"
+              type="text"
+              onChange={(event) => setFirstname(event.target.value)}
+            />
+          </div>
+          <div className="mb-3 d-flex justify-content-around">
+            <label htmlFor="description" className="form-label">
+              Firstname:
+            </label>
+            <input
+              className="form-control w-25"
+              name="lastname"
+              type="text"
+              onChange={(event) => setLastname(event.target.value)}
+            />
+          </div>
+          <div className="mb-3 d-flex justify-content-around">
+            <label htmlFor="description" className="form-label">
+              Email:
+            </label>
+            <input
+              className="form-control w-25"
+              name="email"
+              type="email"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+
+          <div className="mb-3 d-flex justify-content-around">
+            <label htmlFor="description" className="form-label">
+              Address:
+            </label>
+            <input
+              className="form-control w-25"
+              name="address"
+              type="text"
+              onChange={(event) => setAddress(event.target.value)}
+            />
+          </div>
+
+          <div className="mb-3 d-flex justify-content-around">
+            <label htmlFor="description" className="form-label">
+              Password:
+            </label>
+            <input
+              className="form-control w-25"
+              name="password"
+              type="password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div className="mb-3 d-flex justify-content-around">
+            <label htmlFor="description" className="form-label">
+              Phone:
+            </label>
+            <input
+              className="form-control w-25"
+              name="phone"
+              type="text"
+              onChange={(event) => setPhone(event.target.value)}
+            />
+          </div>
+          <input
+            type="file"
+            className="form-control w-50"
+            name="avatar"
+            onChange={(event) => setAvatar(event.target.files[0])}
+          />
+
+          <button className="btn btn-primary" type="submit">
+            Enviar
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default UsersModify;
