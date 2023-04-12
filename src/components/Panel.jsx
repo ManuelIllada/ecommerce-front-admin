a740d88a899ced477ec160cfec3ab18391e1a078
Emi <emi.lng711@gmail.com>
2023-04-12 16:59:16 -0300

    Admin-Style

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -19,7 +19,7 @@ const Panel = () => {
   };
   return (
     <>
-      <div className="d-flex" id="wrapper">
+      <div className="d-flex" style={{ position: "fixed" }}>
         <div className="bg-white" id="sidebar-wrapper">
           <div className="sidebar-heading text-center py-4  fs-4 fw-bold text-dark  text-uppercase border-bottom">
             <i className="me-2">
@@ -78,73 +78,48 @@ const Panel = () => {
               Logout
             </Link>
           </div>
+
+          {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle   fw-bold"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="fas fa-user me-2"></i>
+                  <img
+                    src={userStore.avatar}
+                    className="img-fluid rounded-circle"
+                    alt="avatar user"
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      My Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      href="#"
+                      onClick={handleLogout}
+                    >
+                      Logout
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div> */}
         </div>
-
-        <div id="page-content-wrapper">
-          <nav className="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
-            <div className="d-flex align-items-center">
-              <i className="primary-text fs-4 me-3" id="menu-toggle"></i>
-            </div>
-
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li className="nav-item dropdown">
-                  <Link
-                    className="nav-link dropdown-toggle  fw-bold"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    <i className="fas fa-user me-2"></i>
-                    <img
-                      src={userStore.avatar}
-                      className="img-fluid rounded-circle"
-                      alt="avatar user"
-                      style={{ width: "50px", height: "50px" }}
-                    />
-                  </Link>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li>
-                      <Link className="dropdown-item" href="#">
-                        My Profile
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="dropdown-item"
-                        href="#"
-                        onClick={handleLogout}
-                      >
-                        Logout
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </nav>
-
-          <Outlet />
-        </div>
+      </div>
+      <div className="col" id="page-content-wrapper">
+        <Outlet />
       </div>
     </>
   );
--- src/components/Users/TableUsers.jsx
+++ src/components/Users/TableUsers.jsx
@@ -60,73 +60,76 @@ const TableUsers = () => {
             </Link>
           </div>
         </div>
-
-        <table className="table border rounded">
-          <thead>
-            <tr className="text-center">
-              <th scope="col">#</th>
-              <th scope="col">Avatar</th>
-              <th scope="col"> Full Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {usersList &&
-              usersList.map((user) => (
-                <tr key={user.id} className="text-center">
-                  <td>{user.id}</td>
-                  <td>
-                    {
-                      <img
-                        src={
-                          user.avatar.includes("https://")
-                            ? user.avatar
-                            : `http://localhost:8000/img/${user.avatar}`
-                        }
-                        className="img-fluid rounded-circle"
-                        alt="avatar"
-                        style={{ width: "50px", height: "50px" }}
-                      />
-                    }
-                  </td>
-                  <td>
-                    {user.firstname} - {user.lastname}
-                  </td>
-                  <td>{user.email}</td>
-                  <td className="d-flex justify-content-around">
-                    <Link to="/users/edit" state={user}>
-                      <AiFillEdit className="text-primary" />
-                    </Link>
-                    <Link>
-                      <BsFillTrashFill
-                        className="text-danger"
-                        onClick={(event) =>
-                          Swal.fire({
-                            text:
-                              "Esta seguro que desea eliminar esta categoria?",
-                            icon: "error",
-                            showDenyButton: true,
-                            denyButtonText: "No",
-                            confirmButtonText: "Si",
-                          }).then((response) => {
-                            if (response.isDenied) {
-                              notifyisDenied({
-                                message: "ℹ️ Accion negada por el usuario",
-                                type: "",
-                              });
-                            } else {
-                              handleDelete(event, user);
-                            }
-                          })
-                        }
-                      />
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+        <div className="table-responsive">
+          <table className="table border rounded ">
+            <thead>
+              <tr className="text-center">
+                <th scope="col">#</th>
+                <th className=" d-none d-lg-block" scope="col">
+                  Avatar
+                </th>
+                <th scope="col"> Full Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersList &&
+                usersList.map((user) => (
+                  <tr key={user.id} className="text-center">
+                    <td>{user.id}</td>
+                    <td className=" d-none d-lg-block">
+                      {
+                        <img
+                          src={
+                            user.avatar.includes("https://")
+                              ? user.avatar
+                              : `http://localhost:8000/img/${user.avatar}`
+                          }
+                          className=" img-fluid rounded-circle"
+                          alt="avatar"
+                          style={{ width: "50px", height: "50px" }}
+                        />
+                      }
+                    </td>
+                    <td>
+                      {user.firstname} - {user.lastname}
+                    </td>
+                    <td>{user.email}</td>
+                    <td className="d-flex justify-content-around">
+                      <Link to="/users/edit" state={user}>
+                        <AiFillEdit className="text-primary" />
+                      </Link>
+                      <Link>
+                        <BsFillTrashFill
+                          className="text-danger"
+                          onClick={(event) =>
+                            Swal.fire({
+                              text:
+                                "Esta seguro que desea eliminar esta categoria?",
+                              icon: "error",
+                              showDenyButton: true,
+                              denyButtonText: "No",
+                              confirmButtonText: "Si",
+                            }).then((response) => {
+                              if (response.isDenied) {
+                                notifyisDenied({
+                                  message: "ℹ️ Accion negada por el usuario",
+                                  type: "",
+                                });
+                              } else {
+                                handleDelete(event, user);
+                              }
+                            })
+                          }
+                        />
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
